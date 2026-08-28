f8f45bac3734333bb016231c2030f23f3d4b6b18
user <user@MacBook-Pro-de-user.local>
2026-08-28 23:01:06 +0000

    deuxieme commit

--- app/core/security.py
+++ app/core/security.py
@@ -1,23 +1,21 @@
 from pwdlib import PasswordHash
-import hashlib
 import re
-from passlib.context import CryptContext
 
 
-pwd_context = CryptContext(
-    schemes=["bcrypt"],
-    deprecated="auto"
-)
 
 
-def hash_password(password: str)->str:
-    sha256_hash = hashlib.sha256(password.encode()).hexdigest()
-    bcrypt_hash = pwd_context.hash(sha256_hash)
-    return bcrypt_hash
+password_hash = PasswordHash.recommended()
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    password_bytes = hashlib.sha256(plain_password.encode()).hexdigest() 
-    return pwd_context.verify(password_bytes, hashed_password) 
+
+
+
+
+def hash_password(password: str) -> str:
+    return password_hash.hash(password)
+
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    return password_hash.verify(password, hashed_password)
 
 
 def validate_password_strength(password: str) -> bool:
